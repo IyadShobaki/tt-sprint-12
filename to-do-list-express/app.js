c930d70c14521acb6298401f5c94dc4cb5c4055e
iyadshobaki <iyadshobaki82@gmail.com>
2026-01-18 17:01:27 -0500

    practice: add comments to middleware

--- to-do-list-express/app.js
+++ to-do-list-express/app.js
@@ -5,12 +5,22 @@ const routes = require("./routes");
 const { PORT = 3000, BASE_PATH = "test" } = process.env;
 
 const app = express();
+// Allowing the user to have access to public folder/files
 app.use(express.static(path.join(__dirname, "public")));
 
-// add methods for working with packets here
+// add methods for working with packets here - global middleware function
 app.use(express.json());
+/* express.json() allows an Express application to understand
+ and interpret data sent from clients in a JSON format. */
 app.use(express.urlencoded({ extended: true }));
+/* The express.urlencoded({ extended: true }); middleware is necessary for 
+processing data sent in different formats */
+/* The { extended: true } option allows the middleware 
+to handle more complex types of data. */
 
+/*  // adding a middleware to specific paths examples
+app.use('/users/:id', checkRequest);
+router.get('/users/:id', doesUserExist);*/
 app.use("/", routes);
 
 app.listen(PORT, () => {
